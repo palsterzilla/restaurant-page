0eff2fcd53527a39e059f5cd226d81d14c59566e
Firyal Ghassari <naufal.firyal@gmail.com>
2023-10-20 00:50:50 +0700

    Update index.js linking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import './style.css';
 import slideAnimation from './functions/slideAnimations';
-import home from './pages/home';
-import mainDishes from './pages/mainDishes';
+import * as updatePage from './functions/updatePage'
+import mainDishesPage from './pages/mainDishes';
+import aboutPage from './pages/about';
+
+const pageNav = document.getElementById('menuMyMenu');
 
 slideAnimation()
-home()
-mainDishes()
+
+pageNav.addEventListener('click', (e) => {
+  if (e.target.id === 'home') {
+    updatePage.deleteContent()
+    updatePage.toggleHomeElements(e)
+
+  } else if (e.target.id === 'mainDishes') {
+    updatePage.deleteContent()
+    updatePage.toggleHomeElements(e)
+    mainDishesPage()
+    
+  } else if (e.target.id === 'about') {
+    updatePage.deleteContent()
+    updatePage.toggleHomeElements(e)
+    aboutPage()
+  }
+});
