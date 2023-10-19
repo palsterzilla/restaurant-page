dff52b0f724ece3dac4cd5198abf18b166a299b5
Firyal Ghassari <naufal.firyal@gmail.com>
2023-10-19 20:57:17 +0700

    Add main dishes page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import home from './pages/home';
 import slideAnimation from './functions/slideAnimations';
+import mainDishes from './pages/mainDishes';
 
 // home()
 slideAnimation()
+mainDishes()
